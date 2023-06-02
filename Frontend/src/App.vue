d3e3c49dafaf1f94c110e55384a4563f6816b4b5
vishruti <vishrutik.itpath@gmail.com>
2023-06-02 18:56:26 +0530

    home page design

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
-
+import home from "@/pages/home.vue"
 </script>
 
 <template>
-
-
-
+<home/>
 </template>
 
---
+++ Frontend/src/components/MyBtn.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = withDefaults(defineProps<{
+    title: string,
+    hover: boolean,
+    color: string
+}>(),{
+    title: '',
+    hover: true,
+    color: 'teal-lighten-2'
+}); 
+</script>
+<template>
+    <v-hover>
+        <template v-slot:default="{ isHovering, props }">
+            <v-btn v-bind="props" class="ms-11 ma-5 mt-5 font-weight-bold " size="large" rounded variant="outlined"
+                :class="{ 'bg-teal-lighten-2': isHovering && hover, 'text-teal-lighten-2': !isHovering || !hover,  }">
+                Get Started
+            </v-btn>
+        </template>
+    </v-hover>
+</template>
