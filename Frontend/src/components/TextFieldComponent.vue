0de21e1a8147cf8bf89f23c6572158a075a579d0
Nikulash <nikulashv.itpath@gmail.com>
2023-06-05 13:49:46 +0530

    Fixed some bugs and use route

--- Frontend/src/components/TextFieldComponent.vue
+++ Frontend/src/components/TextFieldComponent.vue
@@ -11,6 +11,11 @@ const props = defineProps({
         type: Boolean,
         default: false
     },
+    isContact:{
+        type:Boolean,
+        default: false
+    },
+    
     prependIcon: {
         type: String,
         required: false
@@ -32,6 +37,10 @@ function rulesFun() {
     if (props.textType == 'email')
         rulArr.push(emailRules)
 
+    if(props.isContact){
+        rulArr.push(contactRules)
+    }
+    
     console.log(rulArr);
     return rulArr
 
@@ -39,9 +48,9 @@ function rulesFun() {
 const showPwd = ref(false)
 const requiredRule = (val: string) => val.trim()=="" ? "Field is Required!" : true
 const emailRules = (value: any) => /.+@.+\..+/.test(value) ? true : 'E-mail must be valid.'
-const noRule = (val: string) => true
 const passwordRule = (val: string) => val.length < 8 ? "At least 8 characters!" : true
-
+const contactRules = (value : string) => /^[0-9]{10}$/.test(value) || value == '' ? true : 'Contact number must be valid'
+const userNameRules = (value: string) => /^[a-zA-Z ]{2,30}$/.test(value) ? true : 'Username must be valid.'
 const emit = defineEmits(['updatedValue']);
 
 const updateValue = (event: InputEvent) => {
@@ -53,9 +62,9 @@ const updateValue = (event: InputEvent) => {
 <template>
     
     <v-text-field :label="props.label" :type="props.textType == 'password' ? showPwd ? 'text' : 'password' : props.textType"
-        :append-icon="props.textType == 'password' ? showPwd ? 'mdi-eye' : 'mdi-eye-off' : ''" :rules="rulesFun()"
-        :prepend-icon="props.prependIcon" @click:append="showPwd = !showPwd"
-        :clearable="props.textType == 'password' ? false : true"
+        :append-inner-icon="props.textType == 'password' ? showPwd ? 'mdi-eye' : 'mdi-eye-off' : ''" :rules="rulesFun()"
+        :prepend-inner-icon="props.prependIcon" @click:append-inner="showPwd = !showPwd"
+        :clearable="props.textType == 'password' ? false : true" variant="outlined"
         @input="updateValue"
         ></v-text-field>
 </template>
--- Frontend/src/pages/signUp.vue
+++ Frontend/src/pages/signUp.vue
@@ -1,28 +1,20 @@
 <script setup lang="ts">
 import { ref, type Ref, reactive } from 'vue';
+import textField from "@/components/TextFieldComponent.vue";
+import { RouterEnum } from '@/enum/RouterEnum';
 const form = ref()
 
-const username: Ref<string>=ref('')
-const email: Ref < string > = ref('')
-const contact: Ref<string>=ref('')
-const password: Ref < string > = ref('')
+const username: Ref<string> = ref('')
+const email: Ref<string> = ref('')
+const contact: Ref<string> = ref('')
+const password: Ref<string> = ref('')
 const confirmPassword: Ref<string> = ref('')
-const passwordShow: Ref<boolean> = ref(false)
-const confirmPasswordShow: Ref < boolean > = ref(false)
-
-const requiredRule = (value: string) => value.trim() == "" ? "Field is Required!" : true
-const userNameRules = (value: string) => /^[a-zA-Z ]{2,30}$/.test(value) ? true : 'UserName must be valid.'
-const emailRules = (value: any) => /.+@.+\..+/.test(value) ? true : 'E-mail must be valid.'
-const contactRules = (value : string) => /^[0-9]{10}$/.test(value) || value == '' ? true : 'Contact number must be valid'
-const passwordRules = (value: string) => value.length < 8 ? "Password must be contain at least 8 characters!" : true
-const confirmPasswordRules = (value:string) => value != password.value ? 'Passwords do not match': true
-            
-async function register() {
+async function register(): Promise<void> {
     const { valid } = await form.value.validate();
     if (valid) {
         alert("Valid")
         form.value.reset();
-    }
+    } 
 }
 </script>
 
@@ -30,31 +22,43 @@ async function register() {
     <v-app>
         <v-main class="d-flex justify-center align-center">
             <v-col cols="12" sm="12" md="6" lg="4" xl="3" xxl="2" class="mx-auto">
-                <v-card class="pa-4">
-                    <div class="text-center mb-3">
-                        <v-avatar size="100" color="teal-lighten-2">
-                            <v-icon size="60" color="">mdi mdi-google-keep</v-icon>
-                        </v-avatar>
-                        <h2 class="indigo--text">SignUp Now!</h2>
-                    </div>
+                <v-card class="pa-4 elevation-12">
+
+                    <v-card-title class="text-center mt-5">
+                        <h2 class="text-teal">Keeper</h2>
+                        Sign Up
+                    </v-card-title>
+                    <v-card-subtitle class="text-center mb-5">
+                        to continue to Keeper
+                    </v-card-subtitle>
+
                     <v-form @submit.prevent="register" ref="form">
-                        <v-card-text>
-                            <v-text-field v-model="username" :rules="[requiredRule, userNameRules]" type="text" label="UserName" placeholder="UserName" prepend-inner-icon="mdi-account" required></v-text-field>
-                            <v-text-field v-model="email" :rules="[requiredRule, emailRules]" type="email" label="Email" placeholder="Email" prepend-inner-icon="mdi-email" required></v-text-field>
-                            <v-text-field v-model="contact" :counter="10" :maxlength="10" :rules="[contactRules]" type="text" label="Contact Number" placeholder="Contact Number" prepend-inner-icon="mdi-account-box"></v-text-field>
-                            <v-text-field v-model="password" :rules="[requiredRule, passwordRules]" :type="passwordShow?'text':'password'" label="Password" placeholder="Password" prepend-inner-icon="mdi-lock" :append-icon="passwordShow ? 'mdi-eye':'mdi-eye-off'" @click:append="passwordShow = !passwordShow"
-                                required></v-text-field>
-                            <v-text-field v-model="confirmPassword" :rules="[requiredRule, confirmPasswordRules]" :type="confirmPasswordShow?'text':'password'" label="Confirm Password" placeholder="Confirm Password" prepend-inner-icon="mdi-key-variant" :append-icon="confirmPasswordShow ? 'mdi-eye':'mdi-eye-off'"
-                                @click:append="confirmPasswordShow = !confirmPasswordShow" required></v-text-field>
-                        </v-card-text>
-                        <v-card-actions class="justify-center">
-                            <v-btn type="submit" color="teal-lighten-2" rounded="lg" size="large" variant="flat">
-                                <span class="px-5">Sign Up</span>
-                            </v-btn>
+
+                        <textField v-model="username" label="Username" placeholder="Username" prepend-icon="mdi-account"
+                            :is-required=true />
+                        <textField v-model="email" text-type="email" label="Email" placeholder="Email"
+                            prepend-icon="mdi-email" :is-required=true />
+                        <textField v-model="contact" :isContact=true :counter="10" :maxlength="10" label="Contact Number"
+                            placeholder="Contact Number" prepend-icon="mdi-account-box" />
+                        <textField v-model="password" text-type="password" label="Password" placeholder="Password"
+                            prepend-icon="mdi-lock" :is-required=true />
+
+                        <textField v-model="confirmPassword" text-type="password" label="Confirm Password"
+                            placeholder="Confirm Password" prepend-icon="mdi-key-variant" :is-required=true />
+
+
+                        <v-card-actions>
+                            <div class="d-flex flex-column justify-center mx-auto">
+                                <v-btn type="submit" flatcolor="#5865f2" rounded="lg" size="large" variant="flat"
+                                    color="teal" class="mt-4">Sign Up</v-btn>
+                                <div class="mt-5">
+                                    Already have an account? <router-link :to="{ name: RouterEnum.SignIn }">Sign
+                                        In</router-link>
+                                </div>
+                            </div>
                         </v-card-actions>
                     </v-form>
-                </v-card>
-            </v-col>
-        </v-main>
-    </v-app>
-</template>
+            </v-card>
+        </v-col>
+    </v-main>
+</v-app></template>
