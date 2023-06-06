bcb89b6a2bf19ca7525633bbddf8934720d04148
Nikulash <nikulashv.itpath@gmail.com>
2023-06-06 18:31:15 +0530

    #25- Make components for all text field

--- Frontend/src/pages/SignInPage.vue
+++ Frontend/src/pages/SignInPage.vue
@@ -2,8 +2,8 @@
 import TextFieldComponent from '@/components/TextFieldComponent.vue';
 import { RouterEnum } from '@/enum/RouterEnum';
 import { ref, type Ref } from 'vue';
-
-
+import TextFieldEmail from "@/components/TextFieldEmail.vue";
+import TextFieldPassword from "@/components/TextFieldPassword.vue";
 const email: Ref<string> = ref('')
 const password: Ref<string> = ref('')
 const form = ref()
@@ -20,11 +20,10 @@ async function login() {
 }
 </script>
 <template>
-    
     <v-app>
         <v-main>
             <v-container fill-height fluid>
-                <v-row justify="center" align-content="center" >
+                <v-row justify="center" align-content="center">
                     <v-col cols="12" lg="4" sm="12">
                         <v-card class="elevation-12 my-auto">
                             <v-card-title class="text-center mt-5">
@@ -36,19 +35,20 @@ async function login() {
                             </v-card-subtitle>
                             <v-card-text>
                                 <v-form ref="form" @submit.prevent="login()">
-                                    
-                                            <text-field-component prepend-icon="mdi-email" label="Email" :is-required=true text-type="email" @updatedValue="(val)=>email=val"/>
-                                            <text-field-component prepend-icon="mdi-lock" label="Password" :is-required=true text-type="password" @updatedValue="(val)=>password=val"/>
+                                    <TextFieldEmail v-model="email" label="Email" />
+                                    <TextFieldPassword v-model="password" label="Password" />
                                     <div class="text-right">
-                                        <router-link :to="{ name: RouterEnum.FORGOTPASSWORD }">Forgot Password?</router-link>
+                                        <router-link :to="{ name: RouterEnum.FORGOTPASSWORD }">Forgot
+                                            Password?</router-link>
                                     </div>
                                     <v-card-actions>
                                         <div class="d-flex flex-column justify-center mx-auto">
                                             <v-btn type="submit" flatcolor="#5865f2" rounded="lg" size="large"
                                                 variant="flat" color="teal" class="mt-4">Login</v-btn>
                                             <div class="mt-5">
-                                                New User? 
-                                                <router-link :to="{ name: RouterEnum.SIGNUP }">Create an account</router-link>
+                                                New User?
+                                                <router-link :to="{ name: RouterEnum.SIGNUP }">Create an
+                                                    account</router-link>
                                             </div>
                                         </div>
                                     </v-card-actions>
