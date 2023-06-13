a4c683409659c4d1ad60a9f623c83c2126b07986
Rucha Gajjar <ruchag.itpath@gmail.com>
2023-06-13 17:47:51 +0530

    Code format

--- Frontend/src/Models/RegisterModel.ts
+++ Frontend/src/Models/RegisterModel.ts
@@ -1,8 +1,7 @@
-export  interface IRegister
-{
-    UserName:string
-    Email:string
-    Contact:string
-    Password:string
-    ConfirmPassword:string
+export interface IRegister {
+    UserName: string
+    Email: string
+    Contact: string
+    Password: string
+    ConfirmPassword: string
 }
