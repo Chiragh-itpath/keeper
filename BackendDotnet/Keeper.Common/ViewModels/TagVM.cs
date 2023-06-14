8b2ba739b486d40714c3b6aa176a38a1fcdc47ab
Nikulash <nikulashv.itpath@gmail.com>
2023-06-14 11:11:39 +0530

    #87 View Model for Tag

---
+++ BackendDotnet/Keeper.Common/ViewModels/TagVM.cs
@@ -0,0 +1,20 @@
+﻿using Keeper.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keeper.Common.ViewModels
+{
+    public class TagVM:IDisposable
+    {
+        public string Title { get; set; }
+        public TagType Type { get; set; }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
