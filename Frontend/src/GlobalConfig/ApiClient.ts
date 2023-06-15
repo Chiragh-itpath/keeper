7a5e71608fffca0d0fda850d7d4ce3c6971e4c19
Nikulash <nikulashv.itpath@gmail.com>
2023-06-15 10:52:11 +0530

    #112 - User interceptors for pass the Token from header

--- Frontend/src/GlobalConfig/ApiClient.ts
+++ Frontend/src/GlobalConfig/ApiClient.ts
@@ -1,5 +1,24 @@
 import axios from "axios";
+import { useCookies } from "vue3-cookies";
 
-export const http=axios.create({
-    baseURL:'https://localhost:7134/api/',
+const { cookies } = useCookies()
+// let token = "";
+// if (cookies.get("token") != null)
+//     token = cookies.get("token")
+// const headers = {
+//     "Authorization": `Bearer ${token}`,
+// };
+export const http = axios.create({
+
+    baseURL: 'https://localhost:7134/api/',
 });
+
+http.interceptors.request.use(
+    config => {
+      config.headers['Authorization'] = `Bearer ${cookies.get("token")}`;
+          return config;
+      },
+      error => {
+          return Promise.reject(error);
+      }
+  );
