f8720a97f93a8f0f94dc078f723db7aabdd3eabd
Nikulash <nikulashv.itpath@gmail.com>
2023-06-15 10:52:58 +0530

    #112- Use axios instance in Tag Service

--- Frontend/src/Services/TagService.ts
+++ Frontend/src/Services/TagService.ts
@@ -1,20 +1,18 @@
 import type { ITag } from "@/Models/TagModel";
 import axios from "axios";
 import type { TagTypeEnum } from "@/enum/TagTypeEnum";
-const token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJKV1RTZXJ2aWNlQWNjZXNzVG9rZW4iLCJqdGkiOiI3MGNmOTNiZS1lNDQ1LTQxZWQtYTc5OS1iOTVkMjE4YTJmNWUiLCJpYXQiOiIwNi8xNC8yMyAxMTo0MDoxOSBBTSIsIklkIjoiNTMzZTQyMzktZDQ1Yy00NDlmLThlODEtZTI4OWM4M2UxNmU0IiwiZXhwIjoxNjg2NzQ2MzU5LCJpc3MiOiJKV1RBdXRoZW50aWNhdGlvblNlcnZlciIsImF1ZCI6IkpXVFNlcnZpY2VQb3N0bWFuQ2xpZW50In0.XrxbiCsy8G09WJ7OLhCaiqLuh-DOCLuByAHDGemUmYM"
-const headers = { 
-    "Authorization": `Bearer ${token}`,
-  };
+import { http } from "@/GlobalConfig/ApiClient";
 export async function Get():Promise<any>{
     try {
-        return await axios.get("https://localhost:7134/api/Tag",{headers:headers});
+        debugger
+        return await http.get("Tag");
     } catch (error) {
         console.log(error);
     }
 }
 export async function GetByType(tagType:TagTypeEnum):Promise<any>{
     try {
-        return await axios.get(`https://localhost:7134/api/Tag/Type/${tagType}`,{headers:headers});
+        return await http.get(`Tag/Type/${tagType}`);
     } catch (error) {
         console.log(error);
     }
@@ -22,7 +20,7 @@ export async function GetByType(tagType:TagTypeEnum):Promise<any>{
 
 export async function GetByTitle(title:string):Promise<any>{
     try {
-        return await axios.get(`https://localhost:7134/api/Tag/Title/${title}`,{headers:headers});
+        return await http.get(`Tag/Title/${title}`);
     } catch (error) {
         console.log(error);
     }
@@ -30,7 +28,7 @@ export async function GetByTitle(title:string):Promise<any>{
 
 export async function Post(tag:ITag):Promise<any>{
     try {
-        return await axios.post(`https://localhost:7134/api/Tag`,tag,{headers:headers});
+        return await http.post(`Tag`,tag);
     } catch (error) {
         console.log(error);
     }
