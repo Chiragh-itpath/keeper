35ab4ff783bfd0b5610d59dbfb287256bdb58e53
Rucha Gajjar <ruchag.itpath@gmail.com>
2023-06-15 12:18:34 +0530

    item page in progress

--- Frontend/src/pages/ItemPage.vue
+++ Frontend/src/pages/ItemPage.vue
@@ -1,4 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Button  from "@/components/ButtonComponent.vue";
+import Card from "@/components/CardComponent.vue";
+</script>
 <template>
-    <h1>items</h1>
+    <v-container>
+        <v-row>
+            <v-col cols="12" md="10" sm="12">
+                <v-text-field type="date" color="primary"></v-text-field>
+            </v-col>
+            <v-col cols="12" md="2" sm="12">
+                <Button variant="elevated" class="my-3" prepend-icon="mdi-plus">Add New Item</Button>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col>
+                <Card>
+                    <template #title>
+                        <v-chip color=""></v-chip>
+                    </template>
+                </Card>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
