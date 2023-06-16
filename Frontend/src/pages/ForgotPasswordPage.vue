d490993033f7d253938fec50e17e56cd099c5947
Rucha Gajjar <ruchag.itpath@gmail.com>
2023-06-16 16:11:04 +0530

    code refactor

--- Frontend/src/pages/ForgotPasswordPage.vue
+++ Frontend/src/pages/ForgotPasswordPage.vue
@@ -31,14 +31,6 @@ async function SubmitForm() {
         <div class="text-h6 text-center text-grey">Please enter your email to recieve a verification code.</div>
         <v-sheet class="mx-auto v-col-12 v-col-lg-6 v-col-sm-12 v-col-md-6">
             <v-form ref="form">
-                <TextFieldEmail label="Enter email" v-model="state.email" color="primary" />
-                <div class="text-right">
-                    <Button class="mb-2" @click="validateEmail()">Send OTP</Button>
-                </div>
-                <TextFieldOTP label="Enter OTP" v-model="state.otp" color="primary" />
-                <div class="text-right">
-                    <Button @click="SubmitForm()" :disabled="state.isDisable" class="mb-3">Submit</Button>
-                </div>
                 <div v-if="state.show">
                     <div class="text-h5 text-center text-primary mb-2">Reset your password</div>
                     <TextFieldPassword label="Enter Password" v-model="state.password" color="primary" class="mb-2">
@@ -48,6 +40,16 @@ async function SubmitForm() {
                     <div class="text-right">
                         <Button @click="SubmitForm()" :disabled="state.isDisable" class="mb-3" type="submit"
                             variant="elevated">Reset password</Button>
+                    </div>
+                </div>
+                <div v-else>
+                    <TextFieldEmail label="Enter email" v-model="state.email" color="primary" />
+                    <div class="text-right">
+                        <Button class="mb-2" @click="validateEmail()">Send OTP</Button>
+                    </div>
+                    <TextFieldOTP label="Enter OTP" v-model="state.otp" color="primary" />
+                    <div class="text-right">
+                        <Button @click="SubmitForm()" :disabled="state.isDisable" class="mb-3">Submit</Button>
                     </div>
                 </div>
             </v-form>
