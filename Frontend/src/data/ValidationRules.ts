136ba4c1cd7ac7982204178ea5c541243a7348ef
Rucha Gajjar <ruchag.itpath@gmail.com>
2023-06-16 16:02:37 +0530

    Added validation rule for OTP. Created OTPTextField component. Created Forgotpassword page.

--- Frontend/src/data/ValidationRules.ts
+++ Frontend/src/data/ValidationRules.ts
@@ -3,11 +3,12 @@ const emailRules = (value: any) => /.+@.+\..+/.test(value) ? true : 'E-mail must
 const passwordRule = (val: string) => val.length < 8 ? "At least 8 characters!" : true
 const contactRules = (value: string) => /^[6-9]{1}[0-9]{9}$/.test(value) || value == '' ? true : 'Contact number must be valid'
 const userNameRules = (value: string) => /^[a-zA-Z ]{2,30}$/.test(value) ? true : 'Username must be valid.'
-
+ const otpRules=(value:string)=>/^[0-9]{6}$/.test(value) ||value==''?true:'OTP must be valid'
 export {
     requiredRule,
     emailRules,
     passwordRule,
     contactRules,
-    userNameRules
+    userNameRules,
+    otpRules
 }
--- Frontend/src/pages/ForgotPasswordPage.vue
+++ Frontend/src/pages/ForgotPasswordPage.vue
@@ -1,1 +1,55 @@
-<template></template>
+<script setup lang="ts">
+import TextFieldEmail from "@/components/TextFieldEmail.vue";
+import Button from "@/components/ButtonComponent.vue";
+import TextFieldOTP from "@/components/TextFieldOTP.vue";
+import TextFieldPassword from "@/components/TextFieldPassword.vue";
+import { reactive, ref } from "vue";
+const form = ref();
+const state = reactive({
+    email: "",
+    otp: "",
+    isDisable: false,
+    password: "",
+    confirmPassword: "",
+    show: false
+});
+async function validateEmail() {
+    const { valid } = await form.value.items[0].validate();
+    if (!valid)
+        return
+};
+async function SubmitForm() {
+    const { valid } = await form.value.validate();
+    if (!valid)
+        return
+    state.show = true;
+};
+</script>
+<template>
+    <v-container>
+        <div class="text-h5 text-center text-primary">Forgot your password?</div>
+        <div class="text-h6 text-center text-grey">Please enter your email to recieve a verification code.</div>
+        <v-sheet class="mx-auto v-col-12 v-col-lg-6 v-col-sm-12 v-col-md-6">
+            <v-form ref="form">
+                <TextFieldEmail label="Enter email" v-model="state.email" color="primary" />
+                <div class="text-right">
+                    <Button class="mb-2" @click="validateEmail()">Send OTP</Button>
+                </div>
+                <TextFieldOTP label="Enter OTP" v-model="state.otp" color="primary" />
+                <div class="text-right">
+                    <Button @click="SubmitForm()" :disabled="state.isDisable" class="mb-3">Submit</Button>
+                </div>
+                <div v-if="state.show">
+                    <div class="text-h5 text-center text-primary mb-2">Reset your password</div>
+                    <TextFieldPassword label="Enter Password" v-model="state.password" color="primary" class="mb-2">
+                    </TextFieldPassword>
+                    <TextFieldPassword label="Enter confirm password" v-model="state.confirmPassword" color="primary"
+                        class="mb-2"></TextFieldPassword>
+                    <div class="text-right">
+                        <Button @click="SubmitForm()" :disabled="state.isDisable" class="mb-3" type="submit"
+                            variant="elevated">Reset password</Button>
+                    </div>
+                </div>
+            </v-form>
+        </v-sheet>
+</v-container></template>
