0929734c3f5189e681a2effefb276a804c554426
Chiragh-itpath <125254659+Chiragh-itpath@users.noreply.github.com>
2023-06-16 14:25:40 +0530

    added identity

--- BackendDotnet/Keeper.Context/Model/UserModel.cs
+++ BackendDotnet/Keeper.Context/Model/UserModel.cs
@@ -1,20 +1,25 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Keeper.Context.Model
 {
     public class UserModel:IDisposable
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
-        [StringLength(30)]
-        public string UserName { get; set; }
-        [StringLength(50)]
-        public string Email { get; set; }
+        [StringLength(30), Required]
+        public string UserName { get; set; } = default!;
+        [StringLength(50), Required]
+        public string Email { get; set; } = default!;
         [StringLength(10)]
-        public string Contact { get; set; }
-        public string Password { get; set; }
+        public string Contact { get; set; } = default!;
+        [Required]
+        public string Password { get; set; } = default!;
         public DateTime CreatedOn { get; set; }
         public DateTime? UpdateOn { get; set; }
-        public virtual IEnumerable<ProjectModel> Projects { get; set; }
-        public virtual IEnumerable<KeepModel> Keeps { get; set; }
+        public virtual IEnumerable<ProjectModel>? Projects { get; set; }
+        public virtual IEnumerable<KeepModel>? Keeps { get; set; }
 
         public void Dispose()
         {
