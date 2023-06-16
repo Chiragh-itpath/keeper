808aaa643f05526eab904e1196d5ee21b530bcdd
Chiragh-itpath <125254659+Chiragh-itpath@users.noreply.github.com>
2023-06-16 18:01:14 +0530

    added virtual model for file upload

---
+++ BackendDotnet/Keeper.Common/ViewModels/FileVM.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Keeper.Common.ViewModels
+{
+    public class FileVM
+    {
+        public List<IFormFile>? Files { get; set; }
+        public Guid UserId { get; set; }
+        public Guid ProjectId { get; set; }
+    }
+}
