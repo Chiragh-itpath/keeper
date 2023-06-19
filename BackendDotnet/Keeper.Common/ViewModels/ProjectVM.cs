69abec19da703465e7684397d690e42137939779
Rucha Gajjar <ruchag.itpath@gmail.com>
2023-06-19 15:44:21 +0530

    added tagid in ProjectVM. Removed dispose() temporarily

--- BackendDotnet/Keeper.Common/ViewModels/ProjectVM.cs
+++ BackendDotnet/Keeper.Common/ViewModels/ProjectVM.cs
@@ -7,21 +7,17 @@ using System.Threading.Tasks;
 
 namespace Keeper.Common.View_Models
 {
-    public class ProjectVM:IDisposable
+    public class ProjectVM
     {
         public Guid Id { get; set; }
         [Required]
         public string Title { get; set; }
+        public Guid? TagId{ get; set; }
         public string? Description { get; set; }
         public Guid CreatedBy { get; set; }
         public Guid UpdatedBy { get; set; }
         public DateTime CreatedOn { get; set; }
         public DateTime UpdatedOn { get; set; }
-
-        public void Dispose()
-        {
-            GC.SuppressFinalize(this);
-        }
     }
 
 }
