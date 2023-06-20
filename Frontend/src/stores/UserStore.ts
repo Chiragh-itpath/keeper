13671a04acd591c57a397da61565c8df7c55258b
Nikulash <nikulashv.itpath@gmail.com>
2023-06-20 13:35:43 +0530

    #165 - Implement Logout functionality

--- Frontend/src/stores/UserStore.ts
+++ Frontend/src/stores/UserStore.ts
@@ -4,9 +4,12 @@ import { defineStore } from 'pinia';
 import { GetUser } from '@/Services/UserService';
 import { computed, ref } from 'vue';
 import { useTokenStore } from '@/stores/TokenStore';
-
+import { useCookies } from 'vue3-cookies';
+import { useRouter } from 'vue-router';
+const router=useRouter()
+const {cookies}=useCookies()
 export const useUserStore = defineStore('user', () => {
-    const User = ref<IUser>();
+    let User = ref<IUser>();
     const { getToken } = useTokenStore();
     async function StoreUser(id: Guid): Promise<void> {
         User.value = await GetUser(id)
@@ -14,11 +17,16 @@ export const useUserStore = defineStore('user', () => {
     const isLoggedin = computed(
         () => User.value != undefined && getToken() != ""
     );
-
+    function logout(){
+        User.value=undefined;
+        cookies.remove("token")
+        router.push("/")
+    }
     return {
         StoreUser,
         User,
-        isLoggedin
+        isLoggedin,
+        logout
     }
 }, {
     persist: true
